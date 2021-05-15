2d5e51d334f425ff931c1c6676cf229918b74bb0
rafael najjar <rafaelnajjar@gmail.com>
2021-05-15 19:17:08 -0300

    one file for each subject, both scss & js

---
+++ js/plantperson.js
@@ -0,0 +1,14 @@
+const plantpersonDesktop = () => {
+    //in Desktop, always make plantperson in the middle of what is left of the background
+    //right-leaf + green background space - plantperson.width/2
+    let a = document.getElementsByClassName("plantperson")[0];
+    a.style.right = `${361 + (window.innerWidth - 1222)/2 - 167}px` 
+}
+
+const plantpersonMobile = () => {
+    //In Mobile, make plantperson appear properly in space
+    let b = document.getElementById("img-plantperson-mobile");
+    b.setAttribute("width", `${593 - (window.innerWidth * 1.286) + 100 }px`);
+}
+
+export { plantpersonDesktop, plantpersonMobile }
--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-document.addEventListener('DOMContentLoaded', function(){ 
-    //in Desktop, always make plantperson in the middle of what is left of the background
-    //right-leaf + green background space - plantperson.width/2
-    let a = document.getElementsByClassName("plantperson")[0];
-    a.style.right = `${361 + (window.innerWidth - 1222)/2 - 167}px` 
+import { plantpersonDesktop, plantpersonMobile } from "/js/plantperson";
 
-
-    //In Mobile, make plantperson appear properly
-    let b = document.getElementById("img-plantperson-mobile");
-    b.setAttribute("width", `${593 - (window.innerWidth * 1.286) + 100 }px`);
-
-    
+document.addEventListener('DOMContentLoaded', function(){
+    plantpersonDesktop();
+    plantpersonMobile();
 }, false);
