d6b2bd48c2b771b5263aa9e21a51023adcc42617
rafael najjar <rafaelnajjar@gmail.com>
2021-05-16 18:31:45 -0300

    big Result part done, now only rest to show plants on screen

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # green-thumb
+
+This is a project created by me, following instructions to reproduce a Figma project.
+
+I wrote all the code in 3 days, and in this README.md file I will explain what I did each day, and what I could do better in this project.
+
+## Intro
+
+The project was meant to be done in plain ***HTML, CSS & JS***, and i couldn't use Frameworks (React, Angular, ...). That was a problem because I'm not used to it, but at the same time it was an interesting opportunity for me to learn a couple of thing in pure JS.
+
+## Day 1 (3h)
+
+* Organized first draft of roadmap (Separated whole project in 3 big parts: Header, Filter & Result)
+* finished Header part.
+
+## Day 2 (5h)
+
+* Finished Filter part. Biggest challenge was for custom dropdown.
+* Organized files. Now each file is for a specific purpose.
+
+## Day 3
+
+## Final Thoughts
+
+The Challenge was fun to do, but some things did bother me: some color and position inconsistencies. This took me more time than I imagined when developing.
+
+The most interesting part for me was doing a custom dropdown from pure JS. That was a blast and I was happy with the result.
--- js/dropdown.js
+++ js/dropdown.js
@@ -1,3 +1,21 @@
+let sunlight;
+const sunlightFormat = {
+    "No sunlight": "no",
+    "Low sunlight": "low",
+    "High sunlight": "high",
+};
+let water;
+const waterFormat = {
+    "Rarely": "rarely",
+    "Regularly": "regularly",
+    "Daily": "daily",
+};
+let chew;
+const chewFormat = {
+    "No/They don't care": "true", //It's pet friendly
+    "Yes": "false", //Pet chew, not pet friendly
+};
+
 const filterLogic = () => {
     document.querySelector('.sunlight').addEventListener('click', function() {
         this.querySelector('.dropdown').classList.toggle('open');
@@ -21,9 +39,8 @@ const filterLogic = () => {
                 this.classList.add('selected');
                 this.closest('.dropdown').querySelector('.dropdown-selected span').textContent = this.textContent;
 
-                console.log(this.textContent);
-
-                //INSERT ONCHANGE LOGIC HERE!!!!!!!!!!!!!
+                //now the onChange logic
+                updateRequest(this.parentNode.parentNode.parentNode.classList[0], this.textContent);
             }
         })
     }
@@ -34,6 +51,46 @@ const filterLogic = () => {
             dropdown.classList.remove('open');
         }
     });
+}
+
+const updateRequest = (field, value) => {
+
+    switch (field) {
+        case "sunlight":
+            sunlight = sunlightFormat[value];
+            break;
+        case "water":
+            water = waterFormat[value];
+            break;
+        case "chew":
+            chew = chewFormat[value];
+            break;
+        default:
+            break;
+    }
+
+    if (sunlight && water && chew) {
+        //if is the first time, change no-result screen to result screen
+        if(document.querySelector('.no-result.open')) {
+            document.querySelector('.no-result').classList.toggle('open');
+            document.querySelector('.result').classList.toggle('open');
+        }
+
+        doRequest();
+    }
+
+}
+
+const doRequest = () => {
+    fetch(`https://front-br-challenges.web.app/api/v2/green-thumb/?sun=${sunlight}&water=${water}&pets=${chew}`)
+    .then(res => res.json())
+    .then(finalResult => {
+        buildDivs(finalResult);
+    })
+}
+
+const buildDivs = (plants) => {
+    console.log(plants);
 }
 
 export { filterLogic };
